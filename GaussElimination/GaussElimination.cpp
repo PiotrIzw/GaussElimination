4277f38a22a0e764fe2c4763a1a9c0169ebf5f0e
PiotrIzw <piotr.izworski.0@gmail.com>
2020-03-25 21:44:26 +0100

    Elimination method implementation

--- GaussElimination/GaussElimination.cpp
+++ GaussElimination/GaussElimination.cpp
@@ -9,12 +9,11 @@ class GaussElimination {
 private:
 	float AB[N][N];
 	int n;
-	string line;
-	fstream file;
 public:
 	void loadData(string filename, float arr[N][N], int n);
 	void loadData(string filename);
 	void eliminate(string fileInputData, string fileMartixSize);
+	void showMatrix(float arr[N][N]);
 };
 
 void GaussElimination::loadData(string filename) {
@@ -37,13 +36,34 @@ void GaussElimination::loadData(string filename, float arr[N][N], int n) {
 void GaussElimination::eliminate(string fileInputData, string fileMartixSize) {
 	loadData(fileMartixSize);
 	loadData(fileInputData, AB, n);
+	
+
+		
+				
 
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n + 1; j++) {
-			cout << AB[i][j] << " ";
+		for (int j = 0; j <= n; j++) {
+			showMatrix(AB);
+			float m = -AB[i + 1][j] / AB[i][j];
+			for (int k = 0; k < n + 1; k++) {
+				
+				AB[i + 1][j] += (AB[i][j] * m);
+			}
+			
 		}
 		cout << endl;
 	}
+}
+
+void GaussElimination::showMatrix(float arr[N][N])
+{
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= n; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
 
 
